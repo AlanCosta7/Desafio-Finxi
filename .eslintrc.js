c58e5266a05e45342c615084a342176c0bca6cc1
Alan Pereira da Costa <alanpc7@gmail.com>
2019-09-17 14:25:46 -0300

    Add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: [
+        'plugin:vue/recommended'
+    ],
+    rules: {
+        "vue/max-attributes-per-line": 0 // depois te explico essa rule (ou não precisa :P)
+    }
+};
