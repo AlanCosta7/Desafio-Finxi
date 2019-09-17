0327ab14df1d7e2c51692f08b176592ad41a4f3c
Alan Pereira da Costa <alanpc7@gmail.com>
2019-09-17 14:57:20 -0300

    Fix: importa semantic-ui-css via webpack (no CDN)

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
       {
         test: /\.css$/,
         use: [
